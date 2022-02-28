c56d2288b42a81023e148129922f7840989fd940
Federico Jorge Bonel Tozzi <Bonelfederico@gmail.com>
2022-02-28 18:03:04 +0900

    Initial Spring Initialization. Closes #1

---
+++ src/main/java/com/fedebonel/recipemvc/RecipeMvcApplication.java
@@ -0,0 +1,13 @@
+package com.fedebonel.recipemvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeMvcApplication.class, args);
+	}
+
+}
