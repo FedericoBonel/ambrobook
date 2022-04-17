a623d5369201fcb23c53c52ebe0f3107ed821f93
Federico Jorge Bonel Tozzi <Bonelfederico@gmail.com>
2022-04-17 18:34:05 +0900

    Added circleci to the project

--- src/test/java/com/fedebonel/recipemvc/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/com/fedebonel/recipemvc/repositories/UnitOfMeasureRepositoryIT.java
@@ -3,9 +3,11 @@ package com.fedebonel.recipemvc.repositories;
 import com.fedebonel.recipemvc.model.UnitOfMeasure;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
@@ -15,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * Integration test of the repository with the spring context and database
  */
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 class UnitOfMeasureRepositoryIT {
 
